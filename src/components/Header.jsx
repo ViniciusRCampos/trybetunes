803e4eb2db7bd16ba15277535866263407e6d566
ViniciusRCampos <vinicius_uo@hotmail.com>
2022-08-30 18:26:30 -0300

    Requisito 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Loading from '../pages/Loading';
 import { getUser } from '../services/userAPI';
 
@@ -25,9 +26,16 @@ export default class Header extends Component {
     const { username, loading } = this.state;
     return (
       <header data-testid="header-component">
-        <h3 data-testid="header-user-name">
-          {loading ? <Loading /> : `${username.name}` }
-        </h3>
+        <section>
+          <h3 data-testid="header-user-name">
+            {loading ? <Loading /> : `${username.name}` }
+          </h3>
+        </section>
+        <section>
+          <Link to="/search" data-testid="link-to-search">Pesquisa</Link>
+          <Link to="/favorites" data-testid="link-to-favorites">Favoritas</Link>
+          <Link to="/profile" data-testid="link-to-profile">Perfil</Link>
+        </section>
       </header>
     );
   }
