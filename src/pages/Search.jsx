8b923c6a5a400484bb5ce38c0b1d605305c29653
ViniciusRCampos <vinicius_uo@hotmail.com>
2022-08-30 18:50:09 -0300

    Requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -3,12 +3,47 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    artistname: '',
+    loading: '',
+    isButtonDisabled: true,
+  };
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+    this.setState({ artistname: value }, () => this.buttonStatus());
+  };
+
+  buttonStatus = () => {
+    const { artistname } = this.state;
+    const minLength = 2;
+    if (artistname.length >= minLength) this.setState({ isButtonDisabled: false });
+  };
+
   render() {
+    const { isButtonDisabled } = this.state
     return (
       <div>
         <Header />
         <div data-testid="page-search">
           Search
+          <form htmlFor="artist-name-input">
+            <input
+              type="text"
+              data-testid="search-artist-input"
+              name="artist-name-input"
+              placeholder="Artist Name"
+              onChange={ this.handleChange }
+            />
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              onClick={ this.handleClick }
+              disabled={ isButtonDisabled }
+            >
+              Pesquisar
+            </button>
+          </form>
         </div>
       </div>
     );
